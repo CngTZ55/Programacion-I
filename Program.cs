096dc20c7b7d9097f83bca8b6c85724dcf648e85
CngTZ55 <78187868+CngTZ55@users.noreply.github.com>
2021-03-19 19:49:44 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,45 +4,113 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Miprimerprograma
+namespace MiPrimerPrograma1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Variables 
-            int a = 5;
-            int b = 9;
-            int c = 3;
 
-            //Números con la forma aacb
+            //Variables
+            String CaracteresDeA = "";
+            String CaracteresDeE = "";
+            Double Altura = 0.0;
+            int Edad = 0;
+
+            //Pidiendo información al usuario
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("Número con la forma: ");
+            Console.Write("¡");
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("aacb");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("{0}{0}{2}{1}", a, b, c);
+            Console.Write("B");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("i");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("e");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("v");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("e");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("i");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("d");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("o");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("s");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" a");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("l");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write(" p");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("a");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("r");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("q");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("u");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("e");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" ");
+            Console.WriteLine("Antes que nada, por favor, indíquenos su edad y estatura a continuación.");
+            
+            //Temporizador 
+            var t = Task.Run(async delegate
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                return 42;
+            });
+            t.Wait();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("¿Su estatura es de cuantos cm? Escriba la estatura de la siguiente manera: ");
+            Console.WriteLine("Ejmeplo 1,60 ");
+            CaracteresDeA = Console.ReadLine();
+            Altura = Convert.ToDouble(CaracteresDeA);
             Console.WriteLine(" ");
 
-            //Números con la forma bca
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("Número con la forma: ");
+            Task.Run(async delegate
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                return 42;
+            });
+
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("bca ");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("{1}{2}{0}", a, b, c);
+            Console.WriteLine("¿Su edad es de?: ");
+            CaracteresDeE = Console.ReadLine();
+            Edad = Convert.ToInt32(CaracteresDeE);
             Console.WriteLine(" ");
 
-            //Números con la forma bcbc
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("Número con la forma: ");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("bcbc ");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("{1}{2}{1}{2}", a, b, c);
-
+            //Filtro de personas en función de las edades
+            if (Edad >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Con una edad de {0} años.", Edad);
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Tienes acceso a todas las atracciones para adultos. Como la La cueva encantada, la Torre T y la Mega Montaña Rusa.");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("OJO:");
+                Console.WriteLine(" No obstante, tienes restringido el acceso a las atracciones infantiles, como el Gusanito, el Carrucel y el Mini-Tren.");
+            }
+            if (Edad < 18 || Altura <= 1.60)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Con una edad de {0} años y una estatura de {1} cm.", Edad, Altura);
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Solo tienes acceso a las atracciones infantiles y adolescentes, como el Gusanito, el Carrucel, el Mini-Tren y el Barco.");
+               
+            }
+       
             Console.ReadLine();
-
+                
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("Miprimerprograma")]
+[assembly: AssemblyTitle("MiPrimerPrograma1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Miprimerprograma")]
+[assembly: AssemblyProduct("MiPrimerPrograma1")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("6402f776-26b1-4ff1-8ffb-0c95baabe33f")]
+[assembly: Guid("06639fff-d6d0-421d-9ab8-fe1632cf857c")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
